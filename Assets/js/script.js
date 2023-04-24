ef015fcaf674e1c650268a6a1b7a9c4e4ab15466
Shubham Rai <shubham.rai@w3villa.com>
2023-04-24 18:32:35 +0530

    update thre code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,11 +1,24 @@
 
-    $('.owl-carousel').owlCarousel({
+    $('#cr').owlCarousel({
     loop:true,
     margin:20,
     nav:false,
+    autoplay:true,
     responsive:{
         0:{
             items:1
+        }
+    }
+})
+
+$('#whyus').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:5
         }
     }
 })
