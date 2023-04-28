f662c02541a2b63451cdb19fceaca298607834f6
Shubham Rai <shubham.rai@w3villa.com>
2023-04-28 15:23:53 +0530

    makeing responsive website

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,117 +1,120 @@
+$("#cr").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+  },
+});
 
-$('#cr').owlCarousel({
-    loop:true,
-    margin:20,
-    nav:false,
-    autoplay:true,
-    responsive:{
-        0:{
-            items:1
-        }
-    }
-})
-
-$('#whyus').owlCarousel({
-    loop:true,
-    margin:20,
-    nav:false,
-    autoplay:true,
-    responsive:{
-        0:{
-            items:1
+$("#whyus").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+//   autoplay: true,
+    responsive: {
+        0: {
+          items: 1
         },
-
-        500:{
-            items:2
-        },
-        1000:{
-            item:4
-        },
-        1200:{
-            items:5
-        }
-       
-
-      
-       
-
-
-        
-    }
-})
-
-
-$('#feature-product').owlCarousel({
-    loop:true,
-    margin:20,
-    nav:false,
-    // autoplay:true,
-    responsive:{
-        0:{
-            items:4
-        }
-    }
-})
-
-$('#feature-categories').owlCarousel({
-    loop:true,
-    margin:20,
-    nav:false,
-    autoplay:true,
-    responsive:{
-        0:{
-            items:4
-        }
-    }
-})
-
-
-$('#shop_brand').owlCarousel({
-    loop:true,
-    margin:20,
-    nav:false,
-    // autoplay:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        400:{
-            items:3
+        426: {
+          items: 2
         },
         700:{
-            items:5
+            items:3
         },
-        900:{
-            items:7
+        900: {
+          items: 4
         },
         1000:{
-            items:9
-        },
-      
-
-    }
-})
-
-$('.blogcr_cr').owlCarousel({
-    loop:true,
-    margin:20,
-    nav:false,
-    autoplay:true,
-    responsive:{
-        0:{
-            items:3
+            items:5
         }
-    }
-})
+      }
+});
 
-$('.saying_about-us').owlCarousel({
-    loop:true,
-    margin:10,
-    // nav:true,
-    responsive:{
-        0:{
-            items:3
-        },
-       
-    }
-})
+$("#feature-product").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+  // autoplay:true,
+  responsive: {
+    0: {
+      items: 4,
+    },
+  },
+});
+
+$("#feature-categories").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 4,
+    },
+  },
+});
+
+$("#shop_brand").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+  // autoplay:true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    300:
+    {
+        items:2,
+    },
+    478:{
+        items:3,
+    },
+    500:{
+        items: 3,
+    },
+    600: {
+      items: 4,
+    },
+    728: {
+      items: 5,
+    },
+    862:
+    {
+        items: 6,
+    },
+    988: {
+      items: 7,
+    },
+    1248: {
+      items: 8,
+    },
+  },
+});
+
+$(".blogcr_cr").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 3,
+    },
+  },
+});
+
+$(".saying_about-us").owlCarousel({
+  loop: true,
+  margin: 10,
+  // nav:true,
+  responsive: {
+    0: {
+      items: 3,
+    },
+  },
+});
