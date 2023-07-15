c7dbc9a158a3b9e9d77f5d5b7dcc3f6f15ba4039
w3villa-shubham-rai <shubham.rai@w3villa.com>
2023-07-15 19:25:06 +0530

    login registration functionality done

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,5 @@
+isUserLogin()
+
 $("#cr").owlCarousel({
   loop: true,
   margin: 20,
@@ -15,23 +17,23 @@ $("#whyus").owlCarousel({
   margin: 20,
   nav: false,
   autoplay: true,
-    responsive: {
-        0: {
-          items: 1
-        },
-        426: {
-          items: 2
-        },
-        700:{
-            items:3
-        },
-        900: {
-          items: 4
-        },
-        1000:{
-            items:5
-        }
-      }
+  responsive: {
+    0: {
+      items: 1
+    },
+    426: {
+      items: 2
+    },
+    700: {
+      items: 3
+    },
+    900: {
+      items: 4
+    },
+    1000: {
+      items: 5
+    }
+  }
 });
 
 
@@ -41,7 +43,7 @@ $("#feature-product").owlCarousel({
   loop: true,
   margin: 20,
   nav: false,
-  autoplay:true,
+  autoplay: true,
   responsive: {
     0: {
       items: 1,
@@ -50,16 +52,16 @@ $("#feature-product").owlCarousel({
       items: 2,
     },
 
-    850:{
-      items:3,
+    850: {
+      items: 3,
     },
-    1126:{
-      items:4,
+    1126: {
+      items: 4,
     },
-    1252:{
-      items:4
+    1252: {
+      items: 4
     }
-   
+
   },
 });
 
@@ -78,18 +80,18 @@ $("#feature-categories").owlCarousel({
     0: {
       items: 0,
     },
-    
-    400:{
-      items:1,
+
+    400: {
+      items: 1,
     },
-    700:{
-      items:2,
+    700: {
+      items: 2,
     },
-    890:{
-      items:3
+    890: {
+      items: 3
     },
-    1100:{
-      items:4
+    1100: {
+      items: 4
     }
   },
 });
@@ -103,23 +105,23 @@ $("#shop_brand").owlCarousel({
   loop: true,
   margin: 5,
   nav: false,
-  autoplay:true,
+  autoplay: true,
   responsive: {
     0: {
       items: 1,
     },
-    200:{
+    200: {
       items: 1,
     },
     300:
     {
-        items:2,
+      items: 2,
     },
-    450:{
-        items:3,
+    450: {
+      items: 3,
     },
-    586:{
-        items: 4,
+    586: {
+      items: 4,
     },
     740: {
       items: 5,
@@ -129,7 +131,7 @@ $("#shop_brand").owlCarousel({
     },
     1032:
     {
-        items: 7,
+      items: 7,
     },
     1100: {
       items: 8,
@@ -150,11 +152,11 @@ $(".blogcr_cr").owlCarousel({
     0: {
       items: 1,
     },
-    800:{
-        items: 2,
+    800: {
+      items: 2,
     },
-    950:{
-        items: 3,
+    950: {
+      items: 3,
     },
   },
 });
@@ -173,15 +175,15 @@ $(".saying_about-us").owlCarousel({
     0: {
       items: 1,
     },
-    300:{
-      items:1,
+    300: {
+      items: 1,
     },
 
-    700:{
-      items:2,
+    700: {
+      items: 2,
     },
-    800:{
-      items:3,
+    800: {
+      items: 3,
     }
   },
 });
@@ -198,94 +200,57 @@ $("#mostviewd").owlCarousel({
     0: {
       items: 1,
     },
-    500:{
+    500: {
       items: 2,
     },
-    940:{
-      items:3
+    940: {
+      items: 3
     },
-    1218:{
-      items:4
+    1218: {
+      items: 4
     }
   },
 });
 
 
 
-// this code for learning purpose
 
-
-// function Electronic_beuty_fashion(elem) {
-  // elem.style.background = "blue"; 
-  
-  // var result=document.getElementsByClassName("btn-group").value;
-  // console.log(result); 
-
-  // document.querySelector(result).className="topcategories";
-
-// }
-
-
-// var header = document.getElementById("myDIV");
-// myFunction_btn_group(header)
-// var header = document.getElementById("myDIV_feature_product");
-// // myFunction_btn_group(header)
-// var header = document.getElementById("myDIV_our_blog");
-// // myFunction_btn_group(header)
-
-
-// function myFunction_btn_group(parent_id) {
-//   var btns = parent_id.getElementsByClassName("btn-group");
-//   for (var i = 0; i < btns.length; i++) {
-//   btns[i].addEventListener("click", function() {
-//   var current = header.getElementsByClassName("active_btn_toogle");
-//   current[0].className = current[0].className.replace(" active_btn_toogle", "");
-//   this.className += " active_btn_toogle";
-//   });
-
-// }
-  
-// }
-function toggleTopCategories(elem){
+function toggleTopCategories(elem) {
   let prevElem = document.getElementsByClassName("active_btn_toogle_categories")[0];
   prevElem.classList.remove("active_btn_toogle_categories");
   elem.classList.add("active_btn_toogle_categories")
 }
- function toogle_featureproduct(elemtop)
- {
-  let previouselement=document.getElementsByClassName("active_btn_toogle_top_categories")[0];
+function toogle_featureproduct(elemtop) {
+  let previouselement = document.getElementsByClassName("active_btn_toogle_top_categories")[0];
   previouselement.classList.remove("active_btn_toogle_top_categories");
   elemtop.classList.add("active_btn_toogle_top_categories");
- }
+}
 
- function toogle_ourblog(blogelement)
- {
-    let previousblogele=document.getElementsByClassName("active_btn_toogle_blog")[0];
-    previousblogele.classList.remove("active_btn_toogle_blog");
-    blogelement.classList.add("active_btn_toogle_blog");
- }
+function toogle_ourblog(blogelement) {
+  let previousblogele = document.getElementsByClassName("active_btn_toogle_blog")[0];
+  previousblogele.classList.remove("active_btn_toogle_blog");
+  blogelement.classList.add("active_btn_toogle_blog");
+}
 
 // login form show logic
 
-function showmudal(self)
-{
+function showmudal(self) {
   document.querySelector('.overlay').classList.add('showlay');
 
   if (self.classList.contains('login')) {
     document.querySelector('#loginform').classList.add('showloginform');
   } else {
-    document.querySelector('#registerform').classList.add('showloginform'); 
+    document.querySelector('#registerform').classList.add('showloginform');
   }
 
 }
 
-function closemudal()
-{
+function closemudal() {
   const loginhide = document.querySelector('#loginform')
   const registerhide = document.querySelector('#registerform')
 
   document.querySelector('.overlay').classList.remove('showlay');
-  
+
   if (loginhide.classList.contains('showloginform')) {
     loginhide.classList.remove('showloginform');
   } else {
@@ -296,145 +261,124 @@ function closemudal()
 }
 
 
-// let a=prompt("enter the key")
-// let b=prompt("enter the value")
+function registerUser(event) {
+  event.preventDefault();
 
-// localStorage.setItem(a,b);
+  var username = document.getElementById('user-name').value;
+  var email = document.getElementById('e-mail-reg').value;
+  var password = document.getElementById('password-type-reg').value;
+  var confirmPassword = document.getElementById('confirm-password').value;
 
-// console.log(`the key is ${a} is ${localStorage.getItem(key)}`)
+  // Check if the username already exists in local storage
+  if (localStorage.getItem(username)) {
+    alert('Username already exists!');
+  } else if (password !== confirmPassword) {
+    alert('Passwords do not match!');
+  } else {
+    let registeredUsers = JSON.parse(localStorage.getItem("users")) || [];
 
-// registration process
+    // Create a new user object
+    var user = {
+      username: username,
+      email: email,
+      password: password
+    };
+    registeredUsers.push(user);
 
-function registeruser()
-{
-  //  let name=document.getElementById('user-name').value;
-  // let username,email,password,confirmpassword
-  // localStorage.setItem("username",username)
-  // localStorage.setItem("email",email)
-  // localStorage.setItem("password",password)
-  // localStorage.setItem("confirm",confirmpassword)
-  let username,email,password,confirmpassword
-  username=document.getElementById("user-name").value,
-  email=document.getElementById("e-mail-reg").value,
-  password=document.getElementById("password-type-reg").value,
-  confirmpassword=document.getElementById("confirm-password").value
+    // Store the user object in local storage
+    localStorage.setItem("users", JSON.stringify(registeredUsers));
+    alert('Registration successful!');
+    closemudal()
+  }
+}
 
-    if(password == confirmpassword)
-    {
-        let dict={
-         "username": username,
-          "email":email,
-          "password":password,
-          "loginstaus":false,
-        }
-      
-      dict = JSON.stringify(dict)  
-      localStorage.setItem("user",dict)
+document.getElementById('registerform').addEventListener('submit', registerUser);
+
+
+
+
+
+
+
+
+// Function to handle login
+function loginFn(event) {
+  event.preventDefault();
+
+  // Get the input values
+  var email = document.getElementById('e-mail').value;
+  var password = document.getElementById('password-type').value;
+
+  // Check if the user exists and the password is correct
+  let user = null;
+  let fetchusersdata = JSON.parse(localStorage.getItem("users"));
+  console.log(fetchusersdata)
+
+  let loggedIn = false;
+  fetchusersdata.forEach(user => {
+    if (user.email === email && user.password === password) {
+      loggedIn = true;
+      localStorage.setItem("user", JSON.stringify(user));
     }
-     else
-     {
-     alert("plese enter the correct password");
-     }
+  });
 
+  if (loggedIn) {
+
+    let userregname = JSON.parse(localStorage.getItem("user")).username;
+    alert('Login successful!');
+    let userlogout = document.getElementById("userlogout");
+    let userregistericon = document.getElementById("userregistericon");
+
+    let displayusername = document.getElementById("dispusername");
+    displayusername.innerHTML = userregname;
+    userlogout.style.display = "block";
+    userregistericon.style.display = "none";
+    closemudal()
+    // showusername_login_reg()
+
+  } else {
+    alert('Invalid email or password!');
   }
+}
 
-  
- 
+// Function to close the modal
+function closeModal() {
+  document.getElementById('loginform').classList.remove('showmodal');
+}
 
-// function login_fn()
-// {
-  
-
-  // let myobj=JSON.parse(localStorage.getItem("user"));
-  // // console.log(myobj);
-  // // console.log(myobj.username,myobj.password);
-  // let usernamelogin=myobj.email;
-  // let userpassword=myobj.password;
-  // let loginGmail=document.getElementById("e-mail").value,
-  // let loginpassword=document.getElementById("password-type").value
- 
-  // if(userpassword == loginpassword && usernamelogin== loginGmail)
-  // {
-  //   alert("user login");
-  // }
-  
-// }
-// let myobj=JSON.parse(localStorage.getItem("user"));
-//   // console.log(myobj);
-//   // console.log(myobj.username,myobj.password);
-//   let usernamelogin=myobj.username;
-//   console.log(usernamelogin);
-//   let userpassword=myobj.password;
-//   console.log(userpassword);
+// Add event listener to the login form
+document.getElementById('loginform').addEventListener('submit', loginFn);
 
 
-function login_fn()
-{
-  let myobj=JSON.parse(localStorage.getItem("user"));
-  let useremailreg=myobj.email;
-  let userregpassword=myobj.password;
-  let loginform=document.getElementById("loginform");
-  let userregname=myobj.username;  
-  let loginstaus=myobj.loginstaus;
-  let logingmail,loginpassword;
-  logingmail=document.getElementById("e-mail").value,
-  loginpassword=document.getElementById("password-type").value
+function showusername_login_reg() {
 
 
-  if(logingmail ==useremailreg &&  loginpassword  ==  userregpassword)
-  {
-    alert("user successfully login");
-    let dict={
-        "username":userregname,
-        "email":useremailreg,
-        "password":userregpassword,
-        "loginstaus":true,
-     }
-   
-      dictlogin = JSON.stringify(dict)  
-      localStorage.setItem("user",dictlogin)
- 
-   
-  }
-  else{
-    alert("user not login");
-  }
- 
+
+
+}
+function logoutuser() {
+
+  // let myobj = JSON.parse(localStorage.getItem("user"));
+  // let status = myobj.loginstaus;
+  localStorage.removeItem("user");
+  let userregistericon = document.getElementById("userregistericon");
+  userregistericon.style.display = "block";
+  let userlogout = document.getElementById("userlogout");
+  userlogout.style.display = "none";
+  let displayusername = document.getElementById("dispusername");
+  displayusername.innerHTML = "login";
 
 }
 
-function showusername_login_reg()
-  {
-    let myobj=JSON.parse(localStorage.getItem("user"));
-    let userregname=myobj.username; 
-    let userlogout=document.getElementById("userlogout");
-    let userregistericon=document.getElementById("userregistericon");
-    
+function isUserLogin (){
+  let user = JSON.parse(localStorage.getItem("user"));
+  if(user){
+    let userlogout = document.getElementById("userlogout");
+    let userregistericon = document.getElementById("userregistericon");
 
-    let status=myobj.loginstaus;
-    if(status==true)
-    {
-      let displayusername=document.getElementById("dispusername");
-      displayusername.innerHTML=userregname;  
-      userlogout.style.display="block";
-      userregistericon.style.display="none";
-    }
-   
-
-  
-   
+    let displayusername = document.getElementById("dispusername");
+    displayusername.innerHTML = user.username;
+    userlogout.style.display = "block";
+    userregistericon.style.display = "none";
   }
-function logoutuser()
-{
-
-  let myobj=JSON.parse(localStorage.getItem("user"));
-  let status=myobj.loginstaus; 
-  let userregistericon=document.getElementById("userregistericon");
-  userregistericon.style.display="block";
-  let userlogout=document.getElementById("userlogout");
-  userlogout.style.display="none";  
-  let displayusername=document.getElementById("dispusername");
-  displayusername.innerHTML="login"; 
-
 }
-
